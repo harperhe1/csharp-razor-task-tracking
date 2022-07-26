6c36c00bfce93230986bb44101b312658418aafd
harperhe1 <harperhe@hotmail.com>
2022-07-26 11:24:32 -0400

    Add Module 4

---
+++ Tasker/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tasker
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
